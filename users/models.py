45b578476eb3d802e98b2fc64c5a1747540078aa
AnuragJha1954 <anuragjha@jklu.edu.in>
2024-08-13 02:20:38 +0530

    edit in plain_password feild

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, unique=True)
     verified = models.BooleanField(default=False)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
-    plain_password = models.CharField(null=True, blank=True)
+    plain_password = models.CharField(max_length=128, null=True, blank=True)
 
     def __str__(self):
         return self.username
